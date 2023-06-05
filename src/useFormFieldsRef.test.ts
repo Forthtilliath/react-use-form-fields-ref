555c5a8dbc19fcaa4061dd978e9e8e417b3c2289
Forthtilliath <forth.developer@gmail.com>
2023-06-05 15:44:14 +0200

    add before all

--- src/useFormFieldsRef.test.ts
+++ src/useFormFieldsRef.test.ts
@@ -1,28 +1,61 @@
 import { renderHook } from "@testing-library/react";
 import { useFormFieldsRef } from "./useFormFieldsRef";
 
+const inputsName = ["username", "password"] as const;
+type InputKey = (typeof inputsName)[number];
+
+type Hook = ReturnType<typeof useFormFieldsRef<InputKey>>;
+let refs: Hook[0];
+let actions: Hook[1];
+
+beforeAll(() => {
+  const { result } = renderHook(() => useFormFieldsRef(inputsName));
+
+  [refs, actions] = result.current;
+});
+
 describe("useFormFieldsRef", () => {
   describe("refs", () => {
     it("should initiate all fields at null", () => {
-      const { result } = renderHook(() => useFormFieldsRef(["username", "password"]));
+      // const { result } = renderHook(() => useFormFieldsRef(["username", "password"]));
 
-      const [refs] = result.current;
+      // const [refs] = result.current;
 
       expect(refs.current).toStrictEqual({
         username: null,
-        password: null
+        password: null,
       });
-      
     });
   });
 
   describe("setRef", () => {
-    it("should set the reference of an input element", () => {
-      // const { result } = renderHook(() => useFormFieldsRef(["username"]));
+    it("should set the reference of a radio input element", () => {
+      // Test that the setRef function sets the reference for a given radio input element
+      const input = document.createElement("input");
+    });
+  });
 
-      // const [refs] = result.current;
+  describe("getRef", () => {
+    it("should return the value contained in the fieldsRef reference for a given key", () => {
+      // Test that the getRef function returns the correct value for a given key
+    });
+  });
 
-      // expect(refs.current.username).toBe(null);
+  describe("getField", () => {
+    it("should return the element contained in the fieldsRef reference for a given key", () => {
+      // Test that the getField function returns the correct element for a given key
+    });
+  });
+
+  describe("getAllRef", () => {
+    it("should return an object containing values from a list of input references", () => {
+      // Test that the getAllRef function returns the correct object containing values from input references
+    });
+  });
+
+  describe("getFormData", () => {
+    it("should return a FormData object with the correct values", () => {
+      // Test that the getFormData function returns the correct FormData object with the input values
     });
   });
 });
